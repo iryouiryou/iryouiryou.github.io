5c9dde297788a8d095b972b91dfb52fe093a7779
iryouiryou <113646589+iryouiryou@users.noreply.github.com>
2023-11-15 16:52:47 +0900

    fixing

--- assets/lunrsearchengine.js
+++ assets/lunrsearchengine.js
@@ -23,9 +23,9 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
 
 var idx = lunr(function () {
     this.use(lunr.ja);
-    this.ref('id')
-    this.field('title')
-    this.field('body')
+    this.ref('id');
+    this.field('title');
+    this.field('body');
 
     documents.forEach(function (doc) {
         this.add(doc)
